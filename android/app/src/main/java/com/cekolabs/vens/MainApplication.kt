2f4706203d922f6757e8160eb412e73ab2bcd17f
monvance <omerfkoca@gmail.com>
2025-07-28 22:20:47 +0300

    rename to vens

--- android/app/src/main/java/com/cekolabs/vens/MainApplication.kt
+++ android/app/src/main/java/com/cekolabs/vens/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.cekolabs.monvance
+package com.cekolabs.vens
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/vens/AppDelegate.swift
+++ ios/vens/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "monvance",
+      withModuleName: "vens",
       in: window,
       launchOptions: launchOptions
     )
