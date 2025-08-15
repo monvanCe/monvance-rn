192ea2aeb726f3b3e9cf93ac02502f3a3ed5fdd6
faruk4234 <faruk.cetin4234@gmail.com>
2025-08-15 15:32:50 +0300

    Add GoogleService-Info.plist for Firebase configuration

--- ios/vens/AppDelegate.swift
+++ ios/vens/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,10 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
+    FirebaseApp.configure()
 
     reactNativeDelegate = delegate
     reactNativeFactory = factory
 
+    
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
